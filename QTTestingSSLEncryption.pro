78d779c3fc8af4aaad7ac9343219dc93fc2eda08
NeroVonDarkstar <ScanlonM1@Cardiff.ac.uk>
2014-05-06 21:20:40 +0100

    All dese things
    Smart.

---
+++ QTTestingSSLEncryption.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2014-04-18T21:22:05
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = QTTestingSSLEncryption
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+LIBS += -lcryptopp
---
+++ main.cpp
@@ -0,0 +1,88 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <QString>
+
+#define CRYPTOPP_DEFAULT_NO_DLL
+#include <cryptopp/dll.h>
+#ifdef CRYPTOPP_WIN32_AVAILABLE
+#include <windows.h>
+#endif
+#include <cryptopp/md5.h>
+USING_NAMESPACE(CryptoPP)
+USING_NAMESPACE(std)
+using namespace std;
+const int MAX_PHRASE_LENGTH=250;
+
+void Save(const string& filename, const CryptoPP::BufferedTransformation& bt)
+{
+    CryptoPP::FileSink file(filename.c_str());
+
+    bt.CopyTo(file);
+    file.MessageEnd();
+}
+
+void SavePublicKey(const string& filename, const CryptoPP::RSA::PublicKey& key)
+{
+    CryptoPP::ByteQueue queue;
+    key.Save(queue);
+
+    Save(filename, queue);
+}
+
+void SavePrivateKey(const string& filename, const CryptoPP::RSA::PrivateKey& key)
+{
+    CryptoPP::ByteQueue queue;
+    key.Save(queue);
+
+    Save(filename, queue);
+}
+
+
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    string messagebuffer = "";
+    //string str("C:/Qt/Tools/QtCreator/bin/QTTestingSSLEncryption/CryptoChatDER.key");
+    //CryptoPP::RSA::PrivateKey prkey;
+    //LoadKey(str, prkey);
+    CryptoPP::AutoSeededRandomPool rnd;
+    CryptoPP::RSA::PrivateKey rsaPrivate;
+    rsaPrivate.GenerateRandomWithKeySize(rnd, 3072);
+
+    CryptoPP::RSA::PublicKey rsaPublic(rsaPrivate);
+
+    SavePrivateKey("CryptoChatRSA.key", rsaPrivate);
+    SavePublicKey("CryptoChatPubRSA.key", rsaPublic);
+
+    cout << "Please enter a message to encrypt" << endl;
+    cin >> messagebuffer;
+    QString PlainMessage = QString::fromStdString(messagebuffer);
+    int plainTextSize = messagebuffer.length();
+
+    string plain="RSA Encryption", cipher, recovered;
+
+    ////////////////////////////////////////////////
+    // Encryption
+    RSAES_OAEP_SHA_Encryptor e(rsaPublic);
+
+    StringSource ss1(plain, true,
+        new PK_EncryptorFilter(rnd, e,
+            new StringSink(cipher)
+       ) // PK_EncryptorFilter
+    ); // StringSource
+
+    ////////////////////////////////////////////////
+    // Decryption
+    RSAES_OAEP_SHA_Decryptor d(rsaPrivate);
+
+    StringSource ss2(cipher, true,
+        new PK_DecryptorFilter(rnd, d,
+            new StringSink(recovered)
+       ) // PK_DecryptorFilter
+    ); // StringSource
+
+    cout << "Recovered plain text" << endl;
+    }
+
+
